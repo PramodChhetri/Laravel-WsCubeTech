fdc29f1d8bc48a4c014bf09379634dd78695b919
Pramod Chhetri <113252695+PramodChhetri@users.noreply.github.com>
2023-12-20 23:21:40 +0545

    Basic of Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo/{name}/{id?}', function ($name, $id = null) {
+    $data = compact('name', 'id');
+    return view('demo')->with($data);
+});
+
+Route::any('/test', function () {
+    echo "Testing";
+});
